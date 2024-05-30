d9728567d3539357b8dc2db49faaaa4587bc0c3f
Olivier Bossaer <olivier.bossaer@gmail.com>
2024-05-30 13:22:23 +0200

    Update credentials for MVP

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,12 +1,34 @@
 import type { Algorithm } from "jsonwebtoken";
 
 const env: ImportMetaEnv = {...import.meta.env, ...process.env};
+
+type DbSettings = { connectionString: string } | { server: string; database: string; };
+
+function getDbSettings(): DbSettings {
+    if (env.DB_CONNECTION_STRING) {
+        return {
+            connectionString: env.DB_CONNECTION_STRING
+        }
+    }
+
+    if (env.DB_SERVER && env.DB_NAME) {
+        return {
+            server: env.DB_SERVER,
+            database: env.DB_NAME
+        }
+    }
+
+    throw new Error("Missing DB Settings.")
+}
+
 export const configuration = Object.freeze({
     msal:{
         applicationId: env.MSAL_APPLICATION_ID,
         tenantId: env.MSAL_APPLICATION_TENANT_ID,
         secret: env.MSAL_APPLICATION_SECRET,
+        requiredGroup: env.MSAL_REQUIRED_GROUP,
     },
+    db: getDbSettings(),
     site: new URL(env.SITE),
     jwt: {
         secret: env.JWT_SECRET,
--- src/middlewares/auth-callback.ts
+++ src/middlewares/auth-callback.ts
@@ -2,7 +2,9 @@ import {defineMiddleware} from 'astro:middleware';
 import { msal, REDIRECT_URI } from '@/services/msal';
 import type { User } from '@/models/user';
 import { login } from './cookie';
+import { configuration } from '@/configuration';
 
+const requiredGroup = configuration.msal.requiredGroup;
 export const authCallbackMiddleware = defineMiddleware(async ({ cookies, locals, redirect, url }, next) => {
     const code = url.searchParams.get('code');
     if (!code || url.pathname !== REDIRECT_URI.pathname) return next();  
@@ -15,8 +17,13 @@ export const authCallbackMiddleware = defineMiddleware(async ({ cookies, locals,
 
     try {
         const response = await msal.acquireTokenByCode(tokenRequest);
+        const idTokenClaims = response.account?.idTokenClaims;
+        if (requiredGroup && idTokenClaims?.groups instanceof Array && !idTokenClaims.groups.includes(requiredGroup)) {
+            return new Response('Access denied', { status: 403 });
+        }
+        
         const user: User = {
-            id: response.account?.idTokenClaims?.oid as string,
+            id: idTokenClaims?.oid as string,
             name: response.account?.name as string
         };
         locals.user = user;
