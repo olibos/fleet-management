1c275d0b31b87149073dca5ccc39cd73f3462f02
Olivier Bossaer <olivier.bossaer@gmail.com>
2024-06-03 20:36:40 +0200

    Fix "Unexpected var, use let or const instead".

--- src/helpers/format-duration.ts
+++ src/helpers/format-duration.ts
@@ -3,9 +3,9 @@ import { twoDigits } from "./two-digits";
 export function formatDuration(start: Date, end: Date) {
     const durationMs = end.getTime() - start.getTime();
 
-    var hours = Math.floor(durationMs / (1000 * 60 * 60));
-    var minutes = Math.floor((durationMs % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((durationMs % (1000 * 60)) / 1000);
+    const hours = Math.floor(durationMs / (1000 * 60 * 60));
+    const minutes = Math.floor((durationMs % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((durationMs % (1000 * 60)) / 1000);
 
     return `${twoDigits(hours)}:${twoDigits(minutes)}:${twoDigits(seconds)}`;
 }
