ae85a359da0e38ae18ccf5bba9bba61ebcc025b9
Olivier Bossaer <olivier.bossaer@gmail.com>
2024-12-09 18:03:21 +0100

    ✨ Small refactoring

---
+++ src/middleware/index.ts
@@ -0,0 +1,7 @@
+import { sequence } from "astro:middleware";
+import { cookieMiddleware } from "./cookie";
+import { authMiddleware } from "./auth";
+import { authCallbackMiddleware } from "./auth-callback";
+import { corsMiddleware } from "./cors";
+
+export const onRequest = sequence(corsMiddleware, cookieMiddleware, authCallbackMiddleware, authMiddleware);
