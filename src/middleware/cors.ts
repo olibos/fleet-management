280795423eae8daef91a354f909995594b3e5fd4
Olivier Bossaer <olivier.bossaer@gmail.com>
2025-01-06 10:09:05 +0100

    Remove this commented out code.

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -4,26 +4,19 @@ export const corsMiddleware = defineMiddleware(async ({ request }, next) => {
     console.info('corsMiddleware');
     const origin = request.headers.get('Origin');
 
-    console.info({origin});
-    return await next();
+    if (request.method === 'OPTIONS') {
+        let headers = new Headers();
+        // TODO: Check origin against a whitelist.
+        headers.append('Access-Control-Allow-Origin', origin ?? '*');
+        headers.append('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+        headers.append('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        return new Response(null, { headers });
+    }
 
-    // if (request.method === 'OPTIONS') {
-    //     let headers = new Headers();
-    //     headers.append('Access-Control-Allow-Origin', '*');
-    //     headers.append('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-    //     headers.append('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    //     return new Response(null, { headers });
-    // }
-
-    // const response = await next();
-
-    // const headers = new Headers(response.headers);
-    // headers.append('Access-Control-Allow-Origin', '*');
-    // headers.append('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-    // headers.append('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    const response = await next();
+    response.headers.set('Access-Control-Allow-Origin', '*');
+    response.headers.set('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+    response.headers.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     
-    // return new Response(response.body, { 
-    //     ...response, 
-    //     headers: headers, 
-    // }); 
+    return response; 
 });
