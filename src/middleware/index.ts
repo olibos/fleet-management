24a81577002ead9dbf57060120cc47763f770c4a
Olivier Bossaer <olivier.bossaer@gmail.com>
2025-01-06 10:09:17 +0100

    Remove this unused import of 'corsMiddleware'.

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,6 +2,6 @@ import { sequence } from "astro:middleware";
 import { cookieMiddleware } from "./cookie";
 import { authMiddleware } from "./auth";
 import { authCallbackMiddleware } from "./auth-callback";
-import { corsMiddleware } from "./cors";
 
-export const onRequest = sequence(/*corsMiddleware, */cookieMiddleware, authCallbackMiddleware, authMiddleware);
+// TODO: Add corsMiddleware.
+export const onRequest = sequence(cookieMiddleware, authCallbackMiddleware, authMiddleware);
